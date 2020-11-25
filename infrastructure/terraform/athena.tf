6b55a4b8751dec0801542a187b523173d0739d42
Will Fenton <willfenton14@gmail.com>
2020-11-25 00:23:57 -0700

    Athena top albums view
    - Created a view in Athena for top albums
    - Includes album name, artist name, top track name,
      first and last listen timestamps, album art url, number of tracks,
      total listen count, and top track listen count

--- infrastructure/terraform/athena.tf
+++ infrastructure/terraform/athena.tf
@@ -3,8 +3,8 @@ resource "aws_athena_database" "database" {
   bucket = aws_s3_bucket.athena_bucket.bucket
 }
 
-resource "aws_athena_named_query" "create_table" {
-  name     = "${lower(replace(var.project_name, "-", "_"))}_create_table_query"
+resource "aws_athena_named_query" "create_table_scrobbles" {
+  name     = "${lower(replace(var.project_name, "-", "_"))}_create_table_scrobbles"
   database = aws_athena_database.database.name
   query    = <<EOF
 CREATE EXTERNAL TABLE IF NOT EXISTS ${aws_athena_database.database.name}.scrobbles (
@@ -21,14 +21,52 @@ TBLPROPERTIES ('has_encrypted_data'='false');
 EOF
 }
 
-resource "aws_athena_named_query" "top_albums" {
-  name     = "${lower(replace(var.project_name, "-", "_"))}_top_albums_query"
+resource "aws_athena_named_query" "create_view_top_albums" {
+  name     = "${lower(replace(var.project_name, "-", "_"))}_create_view_top_albums"
   database = aws_athena_database.database.name
   query    = <<EOF
-SELECT album_name, artist_name, MIN(unix_timestamp), MAX(unix_timestamp), COUNT(*)
-FROM scrobbles
-GROUP BY album_name, artist_name
-ORDER BY COUNT(*) DESC
-LIMIT 200;
+CREATE
+        OR REPLACE VIEW "top_albums" AS
+WITH query1 (album_name, artist_name, count, num_tracks, first_uts, last_uts) AS
+    (SELECT album_name,
+         artist_name,
+         COUNT(*),
+         COUNT(DISTINCT track_name),
+         MIN(unix_timestamp),
+         MAX(unix_timestamp)
+    FROM scrobbles
+    GROUP BY  album_name, artist_name ), query2 (album_name, artist_name, track_name, count, album_art_url) AS
+    (SELECT album_name,
+         artist_name,
+         track_name,
+         COUNT(*),
+         album_art_url
+    FROM scrobbles
+    GROUP BY  album_name, artist_name, track_name, album_art_url ), query3 (album_name, artist_name, track_name, album_count, track_count, num_tracks, rank, first_uts, last_uts, album_art_url) AS
+    (SELECT q2.album_name,
+         q2.artist_name,
+         q2.track_name,
+         q1.count,
+         q2.count,
+         q1.num_tracks,
+         ROW_NUMBER() OVER(PARTITION BY q2.album_name,
+         q2.artist_name
+    ORDER BY  q2.count DESC), q1.first_uts, q1.last_uts, q2.album_art_url
+    FROM ( query1 q1
+    JOIN query2 q2
+        ON q1.album_name = q2.album_name
+            AND q1.artist_name = q2.artist_name ) )
+SELECT album_name,
+         artist_name,
+         track_name,
+         album_count,
+         track_count,
+         num_tracks,
+         first_uts,
+         last_uts,
+         album_art_url
+FROM query3
+WHERE rank = 1
+        AND num_tracks > 2
 EOF
 }
