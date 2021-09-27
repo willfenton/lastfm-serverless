193bcb13fb591c4f3b673b467420d5dadfe9ce88
Will Fenton <wfenton@protonmail.com>
2021-09-27 13:57:20 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-rm -rf build/*
-cp -R src/lambda/*.py build/
-pip install -r src/lambda/requirements.txt --target build/
-cd build
-zip -r lambda-code.zip ./*
+# cd to the script directory https://stackoverflow.com/a/16349776/16790469
+# why is bash the way it is
+CURRENT_DIR=$(pwd)
+
+cd "${0%/*}"
+
+mkdir lambda-build
+rm -rf lambda-build/*
+cp -R src/lambda/*.py lambda-build/
+pip install -r src/lambda/requirements.txt --target lambda-build/
+cd lambda-build
+zip -r -D lambda-code.zip ./*
+cd -
+mv lambda-build/lambda-code.zip .
+rm -rf lambda-build
+
+cd $CURRENT_DIR
